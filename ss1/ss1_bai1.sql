fe5ab6430455542c58eb1a601e8fe3b4352ea242
0961297922a <kimthanh130901@gmail.com>
2023-06-14 13:38:39 +0700

    e nop bai tap

---
+++ ss1/ss1_bai1.sql
@@ -0,0 +1,19 @@
+use my_database;
+create table class(
+class_Id int,
+class_Name varchar(45)
+);
+
+create table Teacher(
+teacher_Id int,
+teacher_Name varchar(45),
+teacher_Age int,
+teacher_Country varchar(45)
+);
+
+create table Student(
+student_Id int,
+student_Name varchar(45),
+student_Age int,
+student_Country varchar(45)
+);

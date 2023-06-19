c209378e98ffcad521547c1a41ab754caf83554e
0961297922a <kimthanh130901@gmail.com>
2023-06-19 21:12:19 +0700

    e nop bai tap

--- ss4/ss4.sql
+++ ss4/ss4.sql
@@ -1,66 +1,66 @@
 create database QuanLySinhVien;
 USE QuanLySinhVien;
 
-CREATE TABLE Class
+CREATE TABLE class
 (
-    ClassID   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ClassName VARCHAR(60) NOT NULL,
-    StartDate DATETIME    NOT NULL,
-    Status    BIT
+    class_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    class_name VARCHAR(60) NOT NULL,
+    start_date DATETIME    NOT NULL,
+    status    BIT
 );
 
-CREATE TABLE Student
+CREATE TABLE student
 (
-    StudentId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    StudentName VARCHAR(30) NOT NULL,
-    Address     VARCHAR(50),
-    Phone       VARCHAR(20),
-    Status      BIT,
-    ClassId     INT         NOT NULL,
-    FOREIGN KEY (ClassId) REFERENCES Class (ClassID)
+    student_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(30) NOT NULL,
+    address     VARCHAR(50),
+    phone       VARCHAR(20),
+    status      BIT,
+    class_id     INT         NOT NULL,
+    FOREIGN KEY (class_id) REFERENCES class (class_id)
 );
 
-CREATE TABLE Subject
+CREATE TABLE subject
 (
-    SubId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    SubName VARCHAR(30) NOT NULL,
-    Credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( Credit >= 1 ),
+    sub_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sub_name VARCHAR(30) NOT NULL,
+    credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( credit >= 1 ),
     Status  BIT                  DEFAULT 1
 );
 
-CREATE TABLE Mark
+CREATE TABLE mark
 (
-    MarkId    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    SubId     INT NOT NULL,
-    StudentId INT NOT NULL,
-    Mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
-    ExamTimes TINYINT DEFAULT 1,
-    UNIQUE (SubId, StudentId),
-    FOREIGN KEY (SubId) REFERENCES Subject (SubId),
-    FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
+    mark_id    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sub_id     INT NOT NULL,
+    student_id INT NOT NULL,
+    mark      FLOAT   DEFAULT 0 CHECK ( mark BETWEEN 0 AND 100),
+    exam_times TINYINT DEFAULT 1,
+    UNIQUE (sub_id, student_id),
+    FOREIGN KEY (sub_id) REFERENCES Subject (sub_id),
+    FOREIGN KEY (student_id) REFERENCES student (student_id)
 );
 
-INSERT INTO Class
+INSERT INTO class
 VALUES (1, 'A1', '2008-12-20', 1);
-INSERT INTO Class
+INSERT INTO class
 VALUES (2, 'A2', '2008-12-22', 1);
-INSERT INTO Class
+INSERT INTO class
 VALUES (3, 'B3', current_date, 0);
 
-INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+INSERT INTO student (student_name, address, phone, status, class_id)
 VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
-INSERT INTO Student (StudentName, Address, Status, ClassId)
+INSERT INTO student (student_name, address, status, class_id)
 VALUES ('Hoa', 'Hai phong', 1, 1);
-INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+INSERT INTO student (student_name, address, phone, status, class_id)
 VALUES ('Manh', 'HCM', '0123123123', 0, 2);
 
-INSERT INTO Subject
+INSERT INTO subject
 VALUES (1, 'CF', 5, 1),
        (2, 'C', 6, 1),
        (3, 'HDJ', 5, 1),
        (4, 'RDBMS', 10, 1);
        
-       INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
+       INSERT INTO mark (sub_id, student_id, mark, exam_times)
 VALUES (1, 1, 8, 1),
        (1, 2, 10, 2),
        (2, 1, 12, 1);
@@ -68,22 +68,22 @@ VALUES (1, 1, 8, 1),
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
 select * 
 from subject 
-where Credit in (
-select max(subject.Credit)
+where credit in (
+select max(subject.credit)
 from subject
 );
 
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
 select *
 FROM subject
-JOIN mark ON subject.SubId = mark.SubId
+JOIN mark ON subject.sub_id = mark.sub_id
 WHERE mark.mark = (SELECT MAX(mark) FROM mark);
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
-SELECT  s.StudentId, s.StudentName, AVG(m.Mark)
+SELECT  s.student_id, s.student_name, AVG(m.Mark)
 FROM student s
-JOIN mark m ON s.StudentId = m.StudentId
-GROUP BY s.StudentId
+JOIN mark m ON s.student_id = m.student_id
+GROUP BY s.student_id
 ORDER BY AVG(m.mark) DESC;
 
 
